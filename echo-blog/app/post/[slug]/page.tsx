6e401ab48c94cee16e8f5efff4a503e08deaef00
suhaslakhani <lakhanisuhas10@gmail.com>
2025-05-22 17:10:46 +0530

    added back to blog

--- echo-blog/app/post/[slug]/page.tsx
+++ echo-blog/app/post/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { posts } from "@/data/post"
 import { notFound } from "next/navigation"
 
@@ -16,12 +17,18 @@ export default function BlogPostPage({ params }:Props){
         <main>
             <section>
                 <div className="max-w-3xl mx-auto p-6">
+                    <Link href="/" className="text-blue-600 hover:underline mb-4 inline-block">
+                        ← Back to Blog
+                    </Link>
                     <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
                     {/* <p className="text-sm text-gray-500 mb-4">By {post.author}</p> */}
                     <p className="text-gray-600 italic mb-6">{post.description}</p>
                     <article className="prose prose-lg text-gray-800 whitespace-pre-line">
                         {post.content}
                     </article>
+                    <Link href="/" className="text-blue-600 hover:underline mb-4 inline-block">
+                        ← Back to Blog
+                    </Link>
                 </div>
             </section>
         </main>
